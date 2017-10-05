322fb47fffa1e74ad6e525576348a10dd98b6c4e
Antonio Contreras <antoniocontreras@Antonios-MacBook-Pro-2.local>
2017-10-05 11:21:02 -0700

    Tested Database class and added premature README

--- src/Database.java
+++ src/Database.java
@@ -1,7 +1,53 @@
+import java.util.HashMap;
+
 /**
  * This will hold all of the table
  * Created by antoniocontreras on 10/5/17.
  */
 public class Database {
+
+    HashMap<String, Table> db;
+    int tableInserterCounter;
+
+    public Database() {
+        db = new HashMap<String, Table>();
+        tableInserterCounter = 0;
+    }
+
+    /*
+     * Inputs the table into the db.
+     */
+    public void insertTable(String name, Table table) {
+        db.put(name, table);
+    }
+
+    public void showAll() {
+        if (db.keySet().size() == 0) {
+            System.out.println("There are no tables in this db");
+        } else {
+            for (String name : db.keySet()) {
+                System.out.println(name);
+            }
+        }
+    }
+
+    /*
+     * Removes the table iff the table exists in the db.
+     */
+    public void removeTable(String tableName) {
+        if (db.keySet().contains(tableName)) {
+            db.remove(tableName);
+        }
+    }
+
+    public static void main(String[] args) {
+        Database db = new Database();
+        Table table = new Table("table1", "Baseball");
+        db.insertTable(table.name, table);
+        db.showAll();
+        db.removeTable("table1");
+        db.showAll();
+        //TODO once test cases for the TABLE class are done make sure to test cross interaction.
+    }
 
 }
--- src/Table.java
+++ src/Table.java
@@ -18,6 +18,9 @@ public class Table {
         table = new HashMap<Integer, ArrayList<String>>();
     }
 
+    /*
+     * For inputs correspond to the NAME, TEAM, POSITIONS and POINTS.
+     */
     public void insertRecord(String name, String team, String position, String points) {
         ArrayList<String> newRecord = new ArrayList<String>();
         newRecord.add(name);
@@ -28,8 +31,15 @@ public class Table {
         uniqueKey++;
     }
 
-    //still deciding how I will want to remove a record
-    public void removeRecord() {
+    /*
+     * At the moment thinking about adding all conditions into a list then iterating
+     * through the list in order to remove them from the table
+     */
+    public void removeRecord(ArrayList<String> lst) {
+        //TODO, how will i remove these
+    }
 
+    public static void main(String[] args) {
+        //TODO think of test cases for the table class.
     }
 }
