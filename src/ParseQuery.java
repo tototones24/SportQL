0da6eab00b5d63189bb1c4fa59b06cc43f40cb9a
Antonio Contreras <antoniocontreras@Antonios-MacBook-Pro-2.local>
2017-10-17 15:51:04 -0700

    Tested single select queries, still need to test conditional queries

--- src/ParseQuery.java
+++ src/ParseQuery.java
@@ -11,14 +11,22 @@ public class ParseQuery {
     String item;
     String location;
     String previousCommand;
+    String conditionItem;
+    String condition;
+    String postCondition;
 
-    //This will parse through the whole query
+
+    /*
+     *This will parse through the whole query and parse all of the
+     * required data needed to extract the information that you need.
+     */
     public boolean  executeQuery(String[] arr, boolean arg, Database db) {
         //TODO: learn how I will parse through the queries
         for (int i = 0 ; i < arr.length; i++) {
 
             //these are single query checks
             //SELECT ____ FROM _______
+
             if (arr[i].equals("end")) {
                 return false;
             } else if (arr[i].toLowerCase().equals("select")) {
@@ -27,20 +35,33 @@ public class ParseQuery {
             } else if (arr[i].toLowerCase().equals("from")) {
                 location = arr[i].toLowerCase();
                 previousCommand = arr[i].toLowerCase();
+            } else if (arr[i].toLowerCase().equals("where")) {
+                previousCommand = arr[i].toLowerCase();
             } else {
-                if (previousCommand == "select") {
+                if (previousCommand.equals("select")) {
                     item = arr[i].toLowerCase();
                     previousCommand = "item";
-                } else if (previousCommand == "from") {
+                } else if (previousCommand.equals("from")) {
                     location = arr[i].toLowerCase();
                     previousCommand = "location";
+                } else if (previousCommand.equals("where")) {
+                    //This will be for query checks with conditions
+                    //First you will have to make it work with equality, then greater than and less thancd 
+                    conditionItem = arr[i].toLowerCase();
+                    previousCommand = "conditionitem";
+                } else if (previousCommand.equals("conditionitem")) {
+                    condition = arr[i].toLowerCase();
+                    previousCommand = "condition";
+                } else if (previousCommand.equals("condition")) {
+                    postCondition = arr[i].toLowerCase();
+                    previousCommand = "postcondition";
                 }
             }
         }
 
         //this is the select clause
         //TODO, test this select statement, simple with NO CONDITIONS
-        if (action == "select") {
+        if (action.equals("select")) {
             return selectClause(item, location, db);
         }
         return true;
@@ -52,18 +73,18 @@ public class ParseQuery {
      */
     public boolean selectClause(String _item, String tableName, Database _db) {
         //selects everything from the corresponding table.
-        if (_item == "*") {
+        if (_item.equals("*")) {
             //this gets the table(hashmap) that i want
             _db.db.get(tableName).showRecords();
             return true;
         } else {
             for (int num : _db.db.get(tableName).table.keySet()) {
                 ArrayList<String> list = _db.db.get(tableName).table.get(num);
-                if (_item == "name") {
+                if (_item.equals("name")) {
                     System.out.println(list.get(0));
-                } else if (_item == "team") {
+                } else if (_item.equals("team")) {
                     System.out.println(list.get(1));
-                } else if (_item == "position") {
+                } else if (_item.equals("position")) {
                     System.out.println(list.get(2));
                 } else {
                     System.out.println(list.get(3));
@@ -74,5 +95,14 @@ public class ParseQuery {
         }
         return true;
     }
+
+
+    /*
+     *This will be a select clause with condistions
+     * SELECT ___ FROM _____ WHERE ________
+     */
+//    public static boolean selectClauseWithCondition() {
+
+//    }
 
 }
--- src/StartDb.java
+++ src/StartDb.java
@@ -6,18 +6,26 @@ import java.util.Scanner;
  */
 public class StartDb {
 
+    //TEST CASES THAT YOU HAVE DONE
+        //* regular select * from table
+
+    //TEST CASE THAT YOU NEED TO DO
+        //you need to check for when spelling is off, when a table does not exist,
 
     public static void main(String[] args) {
         Database newDB = new Database();
         boolean dbActive = true;
+
+        Table table = new Table("Table1", "Baseball");
+        table.insertRecord("Tony", "Yankees", "SS", "186");
+        table.insertRecord("Caleb", "Dodgers", "1B", "00");
+        newDB.insertTable("baseball", table);
+
         while (dbActive) {
 
             Scanner in = new Scanner(System.in);
             String query = in.nextLine();
             String[] tokens = query.split("\\s+");
-
-
-
             ParseQuery parser = new ParseQuery();
             dbActive = parser.executeQuery(tokens, dbActive, newDB);
 
